ceaaf1c4c570bbfad9956de7f17711f22500d859
coolcnid <coolcnid@gmail.com>
2014-02-16 22:50:48 +0000

    add activites file for testing json automatic class converter

--- test/unit/controllerSpec.js
+++ test/unit/controllerSpec.js
@@ -1,12 +1,26 @@
 describe('PostList controllers', function() {
 	describe('PostListCtrl', function() {
 
-		it('should create "posts" with current posts', function(){
+		it('should create "posts" with current posts', function() {
 			var scope = {},
 				ctrl = new PostListCtrl(scope);
 
 			// the length might change ...
 			expect(scope.posts.length).toBe(2);
 		});
+	});
+});
+
+// if the controller function is not in global namespace
+describe('PostList controllers', function() {
+	beforeEach(module('dengs'));
+
+	describe('PostListCtrl', function() {
+		it('should create "posts" with current posts', inject(function($controller) {
+			var scope = {},
+				ctrl = $controller('PostListCtrl', { $scope: scope });
+
+			expect(scope.posts.length).toBe(2);
+		}));
 	});
 });
